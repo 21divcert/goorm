1404064edb646daab7a830693f8b53218bd89f91
21divcert <Hiruca3220@gmail.com>
2024-11-10 15:20:12 +0900

    update: main.py

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
+import csv
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+def load_data():
+    with open('./data/gyul.csv', 'r') as file:
+        reader = csv.DictReader(file, delimiter=",")
+        result = {
+            int(row.pop("year")): row for row in reader
+        }
+    return result
+
+gyul_stats = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global gyul_stats
+    gyul_stats = load_data()
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
     return {"message": "Hello, Jeju!"}
 
+@app.get("/stats")
+async def get_stats():
+    return gyul_stats
+
+@app.get("/stats/{year}")
+async def get_single_year_stats(year: int):
+    return gyul_stats.get(year, {})
